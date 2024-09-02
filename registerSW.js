30d007d70f3f0018122e58318cb354539922e6df
felix9611 <felix9611@users.noreply.github.com>
2024-09-02 21:10:03 +0000

    deploy: 0d75023b3082a1f5d561ff64568796f79ee26670

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/felix9611/sw.js', { scope: '/felix9611/' })})}
