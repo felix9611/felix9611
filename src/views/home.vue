145411b09021ab22e876e672bb2d3fda5787fbc3
Felix <53119101+felix9611@users.noreply.github.com>
2024-09-05 17:42:30 -0400

    new update

--- src/views/home.vue
+++ src/views/home.vue
@@ -1,21 +1,43 @@
-<script setup lang="ts"></script>
-
 <template>
-  <div class="h-sceen py-[9rem]">
-    <div
-      class="lg:px-[10rem] text-center text-[10rem] text-white font-black hover:drop-shadow-lg [text-shadow:_0_5px_10px_rgb(99_102_241_/_0.8)] hover:[text-shadow:_0_10px_15px_#b5edff]"
-    >
-      Felix Page
+  <div class="grid xl:grid-cols-5 px-[5rem] lg:px-[8rem] py-[9rem]">
+    <div class="flex justify-center items-center">
+      <img class="lg:col-sapn-1 rounded-xl scale-150" :src="mySelfHere.photo1" />
     </div>
-    <div
-      class="lg:px-[10rem] text-center text-[5rem] text-white font-black hover:drop-shadow-lg [text-shadow:_0_5px_10px_rgb(99_102_241_/_0.8)] hover:[text-shadow:_0_10px_15px_#b5edff]"
-    >
-      Welcome
+    <div class="h-sceen xl:col-span-3">
+      <div
+        class="lg:px-[10rem] text-center text-[8rem] text-white font-black hover:drop-shadow-lg [text-shadow:_0_8px_10px_rgb(99_102_241_/_0.8)] hover:[text-shadow:_0_10px_15px_#b5edff]"
+      >
+        Felix Page
+      </div>
+      <div
+        class="lg:px-[10rem] text-center text-[5rem] text-white font-black hover:drop-shadow-lg [text-shadow:_0_8px_10px_rgb(99_102_241_/_0.8)] hover:[text-shadow:_0_10px_15px_#b5edff]"
+      >
+        Welcome
+      </div>
+      <div
+        class="lg:px-[10rem] text-center text-[5rem] text-white font-black hover:drop-shadow-lg [text-shadow:_0_8px_10px_rgb(99_102_241_/_0.8)] hover:[text-shadow:_0_10px_15px_#b5edff]"
+      >
+        I'm in there
+      </div>
     </div>
-    <div
-      class="lg:px-[10rem] text-center text-[5rem] text-white font-black hover:drop-shadow-lg [text-shadow:_0_5px_10px_rgb(99_102_241_/_0.8)] hover:[text-shadow:_0_10px_15px_#b5edff]"
-    >
-      I'm in there
+    <div class="flex justify-center items-center">
+      <img class="lg:col-sapn-1 rounded-xl scale-150" :src="mySelfHere.photo2" />
     </div>
   </div>
 </template>
+<script lang="ts">
+import { ref, onMounted, createApp, defineComponent } from 'vue'
+
+const mySelfHere = ref({
+  photo1: '/felix9611/toronto-sakura.jpg',
+  photo2: '/felix9611/maruzensky-2.jpg'
+})
+
+export default defineComponent({
+  setup() {
+    return {
+      mySelfHere
+    }
+  }
+})
+</script>
