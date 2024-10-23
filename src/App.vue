f1ef9dca9d382e330da50a875435718c4984b0ea
Felix <53119101+felix9611@users.noreply.github.com>
2024-10-22 23:09:25 -0400

    style update

--- src/App.vue
+++ src/App.vue
@@ -15,38 +15,40 @@ const footerData = ref({
 
 <template>
   <div class="w-screen h-full bg-gradient-to-r from-[#330831] to-[#134b75] py-4 text-white">
-    <div class="flex gap-5 items-center justify-center grid-col-1 lg:grid-col-3 py-5">
+    <div
+      class="lg:px-[10rem] xl:px-[20rem] text-[1.4rem] flex gap-5 items-center justify-center grid grid-cols-1 lg:grid-cols-4 gap-3 py-5"
+    >
       <button @click="$router.push('/')">
         <div
-          class="text-[1.2rem] text-white font-black hover:drop-shadow-lg [text-shadow:_0_5px_10px_rgb(99_102_241_/_0.8)] hover:[text-shadow:_0_10px_15px_#b5edff]"
+          class="text-white font-black hover:drop-shadow-lg [text-shadow:_0_5px_10px_rgb(99_102_241_/_0.8)] hover:[text-shadow:_0_10px_15px_#b5edff]"
         >
           Home
         </div>
       </button>
       <button @click="$router.push('/about')">
         <div
-          class="text-[1.2rem] text-white font-black hover:drop-shadow-lg [text-shadow:_0_5px_10px_rgb(99_102_241_/_0.8)] hover:[text-shadow:_0_10px_15px_#b5edff]"
+          class="text-white font-black hover:drop-shadow-lg [text-shadow:_0_5px_10px_rgb(99_102_241_/_0.8)] hover:[text-shadow:_0_10px_15px_#b5edff]"
         >
           About Me
         </div>
       </button>
       <button @click="$router.push('/my-photos')">
         <div
-          class="text-[1.2rem] text-white font-black hover:drop-shadow-lg [text-shadow:_0_5px_10px_rgb(99_102_241_/_0.8)] hover:[text-shadow:_0_10px_15px_#b5edff]"
+          class="text-white font-black hover:drop-shadow-lg [text-shadow:_0_5px_10px_rgb(99_102_241_/_0.8)] hover:[text-shadow:_0_10px_15px_#b5edff]"
         >
           Photographs
         </div>
       </button>
       <button @click="$router.push('/my-projects')">
         <div
-          class="text-[1.2rem] text-white font-black hover:drop-shadow-lg [text-shadow:_0_5px_10px_rgb(99_102_241_/_0.8)] hover:[text-shadow:_0_10px_15px_#b5edff]"
+          class="text-white font-black hover:drop-shadow-lg [text-shadow:_0_5px_10px_rgb(99_102_241_/_0.8)] hover:[text-shadow:_0_10px_15px_#b5edff]"
         >
           My Projects
         </div>
       </button>
     </div>
     <div
-      class="py-[1.5rem] text-center text-[3rem] text-white font-black hover:drop-shadow-lg [text-shadow:_0_5px_10px_rgb(99_102_241_/_0.8)] hover:[text-shadow:_0_10px_15px_rgb(99_102_241_/_0.8)]"
+      class="py-[1.65rem] text-center text-[3rem] text-white font-black hover:drop-shadow-lg [text-shadow:_0_5px_10px_rgb(99_102_241_/_0.8)] hover:[text-shadow:_0_10px_15px_rgb(99_102_241_/_0.8)]"
     >
       Here is Felix's Page
     </div>
